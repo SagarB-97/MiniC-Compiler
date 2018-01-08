8efd7ad79bbf843210b0bab20a0b4cb1159e71df
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2018-01-08 16:55:09 +0530

    Initial Compiler - Tokenisation

---
+++ samp_input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*Sample C /*Program
+*/
+*/
+struct samp{
+  long int a;
+  int _bcd_678;
+  short int c;
+  char *str = "Whats Up";
+}
+int main(){
+{
+  struct test1, test2;
+  test1.a = 5;
+
+  test2.a = test1.a + 5645;
+  sendToFunction("Hel  jhklh  kjhgkj lo"");
+  return 0;
+}
